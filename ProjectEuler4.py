f2816503c5e4c10e0d26332d62bb25303268c49d
JRagbeer <30732548+jragbeer@users.noreply.github.com>
2018-05-20 01:25:52 -0400

    Completed

--- ProjectEuler4.py
+++ ProjectEuler4.py
@@ -1,6 +1,12 @@
 import numpy as np
 import pandas as pd
 
+# Project Euler Problem 4
+#
+# A palindromic number reads the same both ways. The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 × 99.
+#
+# Find the largest palindrome made from the product of two 3-digit numbers.
+
 x = y = range(100,1000)
 z = [str(p*q) for p in x for q in y]
 n = [int(x) for x in z if x == x[::-1]]
--- ProjectEuler5.py
+++ ProjectEuler5.py
@@ -1,8 +1,13 @@
 import numpy as np
 import pandas as pd
 
-xx = range(1, 5587021441)
+# Project Euler Problem 5
+#
+# 2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
+#
+# What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
 
+p  = 1*2*3*4*5*7*11*13*17*19*9*16+1
 def check(num):
     count = 0
     for y in range(1,21):
@@ -14,8 +19,6 @@ def check(num):
 
         if count == 20:
             print('WOOOW:    ',num)
-
-
-
+xx = range(1, p)
 for x in xx[::-1]:
     check(x)
