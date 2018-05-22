d4f51a654d128e2307e3ac8247d0971c72926a79
JRagbeer <30732548+jragbeer@users.noreply.github.com>
2018-05-21 22:53:11 -0400

    Completed - Optimized

--- ProjectEuler3.py
+++ ProjectEuler3.py
@@ -1,32 +1,25 @@
 import numpy as np
 import pandas as pd
-import datetime
-# #Project Euler Problem 3
+
+# Project Euler Problem 3
 #
 # The prime factors of 13195 are 5, 7, 13 and 29.
 #
 # What is the largest prime factor of the number 600851475143 ?
-timee = datetime.datetime.now()
-print(timee)
-#y = 13195
-y = 600851475143
 
-facty = [x for x in range(1,y) if y%x == 0]
+def is_prime(n):
+    if n % 2 == 0 or n%3 == 0:
+        return False
+    i = 5
+    while i*i <= n:
+        if n % i == 0 or n % (i + 2) == 0:
+            return False
+        i = i + 6
+    return True
 
-new = []
-for x in facty:
-    for z in facty:
-        if x%z != 0 and x > z:
-            new.append((x,z))
-
-p = {str(facty[x]): len(facty[:x])-1 for x in range(len(facty))}
-
-first = [x[0] for x in new]
-second = [x[1] for x in new]
-
-df = pd.DataFrame(data = {'a':first, 'b':second})
-dat = pd.DataFrame(df.groupby('a').count())
-
-maxx = [x for x in dat.index if p[str(x)] == dat['b'][x]]
-print(max(maxx))
-print(datetime.datetime.now() - timee)
+num = 600851475143
+pp = [i for i in np.arange(np.ceil(np.sqrt(num)))]
+b = [x for x in pp if num%x == 0]
+for y in b:
+    if is_prime(y):
+        print(y)
