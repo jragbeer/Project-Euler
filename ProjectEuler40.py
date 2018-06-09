8b8fac1a0ea348f656021e527759deceb4b2d520
JRagbeer <30732548+jragbeer@users.noreply.github.com>
2018-06-09 11:09:02 -0400

    Completed - Optimized

---
+++ ProjectEuler40.py
@@ -0,0 +1,20 @@
+import numpy as np
+import datetime
+# Project Euler Problem 40
+# An irrational decimal fraction is created by concatenating the positive integers:
+#
+# 0.123456789101112131415161718192021...
+#
+# It can be seen that the 12th digit of the fractional part is 1.
+#
+# If dn represents the nth digit of the fractional part, find the value of the following expression.
+#
+# d1 × d10 × d100 × d1000 × d10000 × d100000 × d1000000
+timee = datetime.datetime.now()
+print(timee)
+p = ['']+[str(i) for i in range(1,500050)]
+d= ''
+for x in p:
+    d = d+ x
+print(int(d[0])*int(d[9])*int(d[99])*int(d[999])*int(d[9999])*int(d[99999])*int(d[999999]))
+print(datetime.datetime.now()-timee)
