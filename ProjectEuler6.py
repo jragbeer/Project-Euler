cf050d7a01ab4d3ebc056399d14cfc549d0489e7
JRagbeer <30732548+jragbeer@users.noreply.github.com>
2018-06-16 02:27:04 -0400

    Completed - Optimized

--- ProjectEuler6.py
+++ ProjectEuler6.py
@@ -12,9 +12,7 @@ import pandas as pd
 # Hence the difference between the sum of the squares of the first ten natural numbers and the square of the sum is 3025 − 385 = 2640.
 #
 # Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
-
 xx = sum([x**2 for x in range(1, 101)])
 yy = sum(range(1,101))**2
-
 print(yy-xx)
 
