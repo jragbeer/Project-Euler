f735a0bd1bb064db84036acffdf910dd93434e12
JRagbeer <30732548+jragbeer@users.noreply.github.com>
2018-06-16 03:03:02 -0400

    Completed - Optimized

--- ProjectEuler7.py
+++ ProjectEuler7.py
@@ -1,24 +1,22 @@
 import numpy as np
-import pandas as pd
+import datetime
 
 # Project Euler Problem 7
 #
 # By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
 #
 # What is the 10 001st prime number?
-#
+timee = datetime.datetime.now()
+def is_prime(n):
+    if n % 2 == 0 or n%3 == 0:
+        return False
+    i = 5
+    while i*i <= n:
+        if n % i == 0 or n % (i + 2) == 0:
+            return False
+        i+=6
+    return True
 a = 2
-xn = range(1,120000000)
-
-for x in range(len(xn)):
-    count = 0
-    for z in range(2,len(xn[:x])):
-        if xn[x]%xn[z] != 0:
-            count = count + 1
-
-        if count == xn[x]-3:
-            a = a + 1
-
-        if a == 10001:
-            print(a, xn[x])
-
+xn = [x for x in range(1,1200000) if is_prime(x)]
+print(xn[9999])
+print(datetime.datetime.now()-timee)
