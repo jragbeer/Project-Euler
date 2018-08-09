8ca0581f164be5fe35136664d944b963f2f78a2f
JRagbeer <30732548+jragbeer@users.noreply.github.com>
2018-08-09 11:59:43 -0400

    Completed

--- ProjectEuler92.py
+++ ProjectEuler92.py
@@ -1,11 +1,17 @@
 import numpy as np
 import pandas as pd
 import datetime
-#Project Euler Problem 1
-
-# If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
-
-# Find the sum of all the multiples of 3 or 5 below 1000.
+#Project Euler Problem 92
+# A number chain is created by continuously adding the square of the digits in a number to form a new number until it has been seen before.
+#
+# For example,
+#
+# 44 → 32 → 13 → 10 → 1 → 1
+# 85 → 89 → 145 → 42 → 20 → 4 → 16 → 37 → 58 → 89
+#
+# Therefore any chain that arrives at 1 or 89 will become stuck in an endless loop. What is most amazing is that EVERY starting number will eventually arrive at 1 or 89.
+#
+# How many starting numbers below ten million will arrive at 89?
 def run():
     zzz = datetime.datetime.now()
     def func1(n):
