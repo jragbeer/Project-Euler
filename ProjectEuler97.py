094e2230bbdf40afcc517ec0a5bcc4afa3cbb74f
JRagbeer <30732548+jragbeer@users.noreply.github.com>
2018-09-08 13:00:56 -0400

    Completed - Optimized

---
+++ ProjectEuler97.py
@@ -0,0 +1,15 @@
+import numpy as np
+import pandas as pd
+import math
+import decimal
+
+# Project Euler Problem 97
+#
+# The first known prime found to exceed one million digits was discovered in 1999, and is a Mersenne prime of the form 26972593−1; it contains exactly 2,098,960 digits. Subsequently other Mersenne primes, of the form 2p−1, have been found which contain more digits.
+#
+# However, in 2004 there was found a massive non-Mersenne prime which contains 2,357,207 digits: 28433×27830457+1.
+#
+# Find the last ten digits of this prime number.
+a = 28433
+b = 2**7830457
+print(str(a*b+1)[-10:])
