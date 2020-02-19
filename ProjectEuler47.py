6b67e95e2de667438401995ce8ef6bf7d436bcc1
JRagbeer <30732548+jragbeer@users.noreply.github.com>
2020-02-19 00:17:47 -0500

    Completed - Optimized

--- ProjectEuler47.py
+++ ProjectEuler47.py
@@ -4,6 +4,7 @@ import datetime
 import cProfile
 import io
 import pstats
+import itertools
 timee = datetime.datetime.now()
 # The first two consecutive numbers to have two distinct prime factors are:
 #
@@ -17,7 +18,6 @@ timee = datetime.datetime.now()
 # 646 = 2 × 17 × 19.
 #
 # Find the first four consecutive integers to have four distinct prime factors each. What is the first of these numbers?
-
 
 def profile(fnc):
     """A decorator that uses cProfile to profile a function"""
@@ -35,58 +35,32 @@ def profile(fnc):
         return retval
 
     return inner
-def is_prime(n):
-    if n % 2 == 0 or n%3 == 0:
-        return False
-    i = 5
-    while (i*i <= n):
-        if n % i == 0 or n% (i+2) == 0:
-            return False
-        i = i + 6
-    return True
 @profile
-def main():
-    bignum = 500000
-
-    a = [2, 3, 4] + [x for x in range(2, int(bignum / 8)) if is_prime(x)]
-
-    print(a)
-    new_list = dict()
-    gotcha = []
-    for x in a:
-        for y in a:
-            if x * y < bignum / 4 + 1:
-                if x != y:
-                    for z in a:
-                        if x * y * z < bignum / 2 + 1:
-                            if y != z:
-                                if z != x:
-                                    for q in a:
-                                        if q not in [x, y, z]:
-                                            www = x * y * z * q
-                                            if www < bignum:
-                                                for i in range(bignum):
-                                                    if www == i:
-                                                        new_list[i] = {1: x, 2: y, 3: z, 'num': i}
-                                                        try:
-                                                            if new_list[i - 3]['num'] - new_list[i - 4]['num'] == 1:
-                                                                if new_list[i - 2]['num'] - new_list[i - 3]['num'] == 1:
-                                                                    if new_list[i - 1]['num'] - new_list[i - 2]['num'] == 1:
-                                                                        if new_list[i]['num'] - new_list[i - 1]['num'] == 1:
-                                                                            k = sorted([i, i - 1, i - 2, i - 3, i - 4])
-                                                                            # print(k)
-                                                                            if k not in gotcha:
-                                                                                gotcha.append(k)
-                                                                                thing = []
-                                                                                for t in k:
-                                                                                    thing.append(list(new_list[t].values()))
-                                                                                ok = [o for ttt in thing for o in ttt]
-                                                                                # print(ok)
-                                                                                if len(ok) == len(list(set(ok))):
-                                                                                    print(np.min(k))
-                                                        except:
-                                                            pass
-
-    print(datetime.datetime.now() - timee)
-
-main()
+def func():
+    def is_prime(n):
+        if n % 2 == 0 or n % 3 == 0:
+            return False
+        i = 5
+        while (i * i <= n):
+            if n % i == 0 or n % (i + 2) == 0:
+                return False
+            i = i + 6
+        return True
+    def factors(n):
+        # find factors, and check if they're in the list of primes
+        t = [x for x in sorted(list(set(itertools.chain.from_iterable((i, n // i)
+                                                     for i in range(1, int(n ** 0.5) + 1) if n % i == 0)))) if x in a]
+        # if 2 and 4 are in list, only use 4 as the prime
+        if 2 and 4 in t:
+            t.remove(2)
+        return t
+    bignum = 150000
+    a = [2, 3, 4] + [x for x in range(2, int(bignum / 4)) if is_prime(x)]
+    right = [z for z in range(bignum) if len(factors(z)) >= 4]
+    for x in range(len(right)):
+        if right[x]+1 == right[x+1]:
+            if right[x] + 2 == right[x + 2]:
+                if right[x] + 3 == right[x + 3]:
+                    print(right[x])
+                    break
+func()
