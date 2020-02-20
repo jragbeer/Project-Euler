959aa9b3881bbf6f8389be00161b7202d80039c1
JRagbeer <30732548+jragbeer@users.noreply.github.com>
2020-02-20 11:54:16 -0500

    Completed - Optimized

---
+++ ProjectEuler73.py
@@ -0,0 +1,57 @@
+import numpy as np
+import cProfile
+import io
+import datetime
+import itertools
+import pstats
+from tqdm import tqdm
+from pprint import pprint
+import math
+
+# Project Euler Problem 71
+# Consider the fraction, n/d, where n and d are positive integers. If n<d and HCF(n,d)=1, it is called a reduced proper fraction.
+#
+# If we list the set of reduced proper fractions for d ≤ 8 in ascending order of size, we get:
+#
+# 1/8, 1/7, 1/6, 1/5, 1/4, 2/7, 1/3, 3/8, 2/5, 3/7, 1/2, 4/7, 3/5, 5/8, 2/3, 5/7, 3/4, 4/5, 5/6, 6/7, 7/8
+#
+# It can be seen that 2/5 is the fraction immediately to the left of 3/7.
+#
+# By listing the set of reduced proper fractions for d ≤ 1,000,000 in ascending order of size, find the numerator of the fraction immediately to the left of 3/7.
+
+def profile(fnc):
+    """A decorator that uses cProfile to profile a function"""
+
+    def inner(*args, **kwargs):
+        pr = cProfile.Profile()
+        pr.enable()
+        retval = fnc(*args, **kwargs)
+        pr.disable()
+        s = io.StringIO()
+        sortby = 'cumulative'
+        ps = pstats.Stats(pr, stream=s).sort_stats(sortby)
+        ps.print_stats()
+        print(s.getvalue())
+        return retval
+
+    return inner
+
+@profile
+def run():
+    max_fraction = 1/2
+    min_fraction = 1/3
+    d = 12000
+    output = []
+    for x in tqdm(range(1, d+1)):
+        for y in range(1, int(x/2)+1):
+            if x%2 == 0:
+                if y%2 == 0:
+                    continue
+            if math.gcd(x, y) == 1:
+                c = y/x
+                if min_fraction < c < max_fraction:
+                    output.append(c)
+    output.sort()
+    print(output[:3], output[-3:])
+    print(len(output))
+run()
